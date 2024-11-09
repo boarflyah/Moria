8d5974fb3ae0360d08926e8ad49493be2cec746f
Arek Stepnik <arkadiusz.stepnik88@gmail.com>
2024-11-09 22:00:23 +0100

    Added basic configuration (logging, getting ipaddress and port number to listen) to MoriaWebAPI

---
+++ MoriaWebAPI/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoriaWebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController: ControllerBase
+{
+    readonly ILogger<TestController> _logger;
+
+    public TestController(ILogger<TestController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        _logger.LogCritical($"Testowy log: {nameof(TestController)}");
+
+        return Ok();
+    }
+}
--- MoriaWebAPI/Program.cs
+++ MoriaWebAPI/Program.cs
@@ -1,32 +1,73 @@
+using System.Net;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Serilog;
+
 namespace MoriaWebAPI;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        try
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Host.UseWindowsService();
+
+            //logger configuration
+            Log.Logger = new LoggerConfiguration()
+                //.MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(".\\Logs\\log-.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+            builder.Logging.ClearProviders();
+            builder.Logging.AddSerilog(Log.Logger);
+
+            // Add services to the container.
+
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
+
+            builder.WebHost.ConfigureKestrel((context, options) =>
+            {
+                var ip = GetLocalIPAddress();
+                var portNumber = builder.Configuration.GetValue(typeof(int), "PortNumber") as int?;
+                options.Listen(IPAddress.Parse(ip), portNumber.GetValueOrDefault(5000), configure =>
+                {
+                    configure.UseHttps();
+                });
+            });
+
+            var app = builder.Build();
+
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.MapControllers();
+
+            app.Run();
         }
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
+
+    static string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+            if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                return ip.ToString();
+        throw new Exception("No network adapters with an IPv4 address in the system!");
+    }
+
 }
